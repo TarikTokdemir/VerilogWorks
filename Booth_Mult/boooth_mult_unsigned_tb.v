4ef90fbf7c910d52d6be459461763ee79467166a
TarikTokdemir <tariktokdemir08@gmail.com>
2025-03-24 13:47:40 +0300

    Verified
    if you want to multiply two 8 bit numbers and they are only positive , you must you need to process them as 9 bits . That way you can test the extremes (like 255x1, 255x0, 1x255).

---
+++ Booth_Mult/boooth_mult_unsigned_tb.v
@@ -0,0 +1,97 @@
+`timescale 1ns / 1ps
+
+module booth_mult_unsigned_tb;
+
+parameter DATA_WIDTH = 8;
+
+reg clk_i_mult, rstn_i_mult, en_i_mult;
+reg [DATA_WIDTH-1:0] A, B;
+wire [DATA_WIDTH*2-1:0] result_o;
+wire mult_done_o;
+    
+    booth_mult_unsigned #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) uut (
+        .clk_i_mult(clk_i_mult),
+        .rstn_i_mult(rstn_i_mult),
+        .en_i_mult(en_i_mult),
+        .A(A),
+        .B(B),
+        .result_o(result_o),
+        .mult_done_o(mult_done_o)
+    );
+    
+    initial begin
+        clk_i_mult = 0;
+        forever #5 clk_i_mult = ~clk_i_mult;
+    end
+    
+    initial begin
+        rstn_i_mult = 0;
+        en_i_mult   = 0;
+        A           = 0;
+        B           = 0;
+        
+        #20;
+        rstn_i_mult = 1;  
+        #10;
+        
+        A = 8'd3;
+        B = 8'd5;
+        en_i_mult = 1;  
+        #10;
+        
+        wait (mult_done_o == 1);
+        $display("Test 1: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+        
+        #20;
+        en_i_mult = 1;
+        A = 8'd7;
+        B = 8'd9;
+        
+        wait (mult_done_o == 1);
+        $display("Test 2: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+        
+        #20;
+        en_i_mult = 1;
+        A = 8'd255;
+        B = 8'd1;
+        
+        wait (mult_done_o == 1);
+        $display("Test 2: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+        
+        #20;
+        en_i_mult = 1;
+        A = 8'd255;
+        B = 8'd0;
+        
+        wait (mult_done_o == 1);
+        $display("Test 2: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+        
+        #20;
+        en_i_mult = 1;
+        A = 8'd0;
+        B = 8'd255;
+        
+        wait (mult_done_o == 1);
+        $display("Test 2: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+        
+        #20;
+        en_i_mult = 1;
+        A = 8'd255;
+        B = 8'd254;
+        
+        wait (mult_done_o == 1);
+        $display("Test 2: A = %d, B = %d, result = %d", A, B, result_o);
+        en_i_mult = 0 ;
+
+        #20;
+        $finish;//
+    end
+
+endmodule
--- Booth_Mult/booth_mult_unsiged.v
+++ Booth_Mult/booth_mult_unsiged.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module booth_mult #(
+module booth_mult_unsigned #(
     parameter DATA_WIDTH = 8 
 )(
 input                       clk_i_mult  , rstn_i_mult , en_i_mult ,
@@ -22,13 +22,13 @@ localparam  SUB     = 2'b10,
             SUM     = 2'b01;
 
             
-reg [DATA_WIDTH-1:0] multiplier     ; // A 
-reg [DATA_WIDTH-1:0] multiplicand   ; // B
-reg [DATA_WIDTH-1:0] Zeros          ; 
+reg [DATA_WIDTH:0] multiplier     ; // A 
+reg [DATA_WIDTH:0] multiplicand   ; // B
+reg [DATA_WIDTH:0] Zeros          ; 
 reg                  Q_1            ;
-reg signed [DATA_WIDTH*2:0] temp17         ;
+reg signed [DATA_WIDTH*2+2:0] temp19         ;
 reg [DATA_WIDTH/2:0] counter        ;
-reg [DATA_WIDTH  :0] complement_multiplicand ; 
+reg [DATA_WIDTH+1  :0] complement_multiplicand ; 
 
 reg [DATA_WIDTH*2-1:0] result_o_reg ;
 assign result_o = result_o_reg; 
@@ -58,28 +58,28 @@ always @(posedge clk_i_mult or posedge rstn_i_mult) begin
             end
             
             LOAD : begin // 1 
-                multiplier      <= A ;
-                multiplicand    <= B ;
-                temp17 <= {Zeros , A , Q_1};  // 8 8 1
+                multiplier      <= {1'b0 , A} ;
+                multiplicand    <= {1'b0 , B} ;
+                temp19 <= {Zeros , A , Q_1};  // 9 9 1
                 complement_multiplicand <= ( ((~complement_multiplicand) + 1'b1) ) ;  
                 STATE <= OP ; 
             end
             
             OP : begin // 2
-                if (counter < DATA_WIDTH ) begin
-                    case ({temp17[1] , temp17[0]}) 
+                if (counter < DATA_WIDTH+1 ) begin
+                    case ({temp19[1] , temp19[0]}) 
                         SUM : begin // 01
-                            temp17[DATA_WIDTH*2:DATA_WIDTH+1] <= temp17[DATA_WIDTH*2:DATA_WIDTH+1] + multiplicand ; 
+                            temp19[DATA_WIDTH*2+2:DATA_WIDTH+2] <= temp19[DATA_WIDTH*2+2:DATA_WIDTH+2] + multiplicand ; 
                             STATE <= SHIFT ; 
                         end
                         
                         SUB : begin // 10 
-                            temp17[DATA_WIDTH*2:DATA_WIDTH+1] <= temp17[DATA_WIDTH*2:DATA_WIDTH+1] + complement_multiplicand[DATA_WIDTH-1:0] ; 
+                            temp19[DATA_WIDTH*2+2:DATA_WIDTH+2] <= temp19[DATA_WIDTH*2+2:DATA_WIDTH+2] + complement_multiplicand[DATA_WIDTH:0] ; 
                             STATE <= SHIFT ; 
                         end
                         
                         default : begin
-                            temp17 <= temp17 >>> 1 ; 
+                            temp19 <= temp19 >>> 1 ; 
                         end
                     endcase
                     counter <= counter + 1 ; 
@@ -90,13 +90,13 @@ always @(posedge clk_i_mult or posedge rstn_i_mult) begin
             end 
             
             SHIFT : begin // 3
-                temp17 <= {temp17[16], temp17[16:1]};
+                temp19 <= temp19 >>> 1 ;
                 STATE   <= OP ; 
             end
             
             LAST : begin // 4 
                 mult_done_o_reg <= 1 ; 
-                result_o_reg    <= temp17[DATA_WIDTH*2:1] ; 
+                result_o_reg    <= temp19[DATA_WIDTH*2+2:1] ; 
                 STATE           <= IDLE ;
             end
         endcase
