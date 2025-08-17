08ed2e8874fc2a1b89b49184e744733464bdf930
tariktokdemir <tariktokdemir08@gmail.com>
2025-08-17 04:19:24 +0300

    Add files via upload

--- Mentoring_Apps_2025/6-) Half_Subtractor/half_subtractor.v
+++ Mentoring_Apps_2025/6-) Half_Subtractor/half_subtractor.v
@@ -1,1 +1,13 @@
+`timescale 1ns / 1ps
 
+module half_subtractor(
+    input wire A , B ,
+    
+    output wire Difference,
+    output wire Borrow  
+    );
+    
+    assign Difference = A ^ B  ; 
+    assign Borrow     = ~A & B ; 
+    
+endmodule
