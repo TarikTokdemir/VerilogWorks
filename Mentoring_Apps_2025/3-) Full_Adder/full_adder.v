8e64f9ee22a592e1a79a5ac192b919aa94055a27
tariktokdemir <tariktokdemir08@gmail.com>
2025-08-17 04:10:21 +0300

    Add files via upload

--- Mentoring_Apps_2025/3-) Full_Adder/full_adder.v
+++ Mentoring_Apps_2025/3-) Full_Adder/full_adder.v
@@ -1,1 +1,37 @@
+`timescale 1ns / 1ps
 
+
+module full_adder(
+    input wire  a   ,
+    input wire  b   , 
+    input wire  cin ,
+    output wire sum , 
+    output wire cout
+    
+    );
+    
+    wire sum1 , carry1 , carry2 ;
+    
+    half_adder ha1 (
+        //  atama yönü sola doðru <--
+        .a    (a        ) ,
+        .b    (b        ) ,
+        
+        // --> yön saða 
+        .sum  (sum1     )  ,   //outputtu alt modulde
+        .carry(carry1   )    //outputtu alt modulde
+    );
+    
+    
+    half_adder ha2 (
+        .a (sum1)        ,
+        .b (cin)         ,
+        
+        .sum   (sum)     ,
+        .carry (carry2)
+    );
+    
+    
+    assign cout = carry1  |  carry2  ;
+    
+endmodule
